683e6fe42d2c741df58c6edbc50745f61354cca0
BehzadIqbal <iqbalbehzad@gmail.com>
2018-12-27 16:12:51 +0100

    List met karweitjes

--- app/src/main/java/com/acosta/ricardo/logindemo/Werknemer.java
+++ app/src/main/java/com/acosta/ricardo/logindemo/Werknemer.java
@@ -2,12 +2,20 @@ package com.acosta.ricardo.logindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Werknemer extends AppCompatActivity {
+    ListView mListView;
+    String[] karweitjes = {"Auto wassen","Babysitten","Gras maaien","Hond uitlaten","Kast monteren",
+            "Computer Repareren","Photoshoot","De afwas doen", "Kamer schilderen","Stopcontact vervangen"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_werknemer);
+        mListView = (ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        mListView.setAdapter(mAdapter);
     }
 }
