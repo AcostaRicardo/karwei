ddb6cc5c111e51c5f72245624ee5e51971e5dbc9
Ricardo Acosta <ricardo.acostatorres@student.ap.be>
2018-12-31 15:42:35 +0100

    List & Firebase
    de list krijgt data van de firebase server

--- app/src/main/java/com/acosta/ricardo/logindemo/Karwei.java
+++ app/src/main/java/com/acosta/ricardo/logindemo/Karwei.java
@@ -1,25 +1,52 @@
 package com.acosta.ricardo.logindemo;
 
 public class Karwei {
-    String title;
-    String description;
-    String contact_info;
+    private String title;
+    private String description;
+    private String contact;
+    private String  wage;
 
-    public Karwei(String title, String description, String contact_info){
+    //CONSTRUCTOR
+    public Karwei(String title, String description, String contact, String wage) {
         this.title = title;
         this.description = description;
-        this.contact_info = contact_info;
+        this.contact = contact;
+        this.wage = wage;
+    }
+    //CONSTRUCTOR
+    public Karwei() {
     }
 
-    public String getTitle(){
+    //GETTERS & SETTERS
+    public String getTitle() {
         return title;
     }
 
-    public String getDescription(){
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
         return description;
     }
 
-    public String getContact_info(){
-        return contact_info;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getWage() {
+        return wage;
+    }
+
+    public void setWage(String wage) {
+        this.wage = wage;
     }
 }
--- app/src/main/java/com/acosta/ricardo/logindemo/Werknemer.java
+++ app/src/main/java/com/acosta/ricardo/logindemo/Werknemer.java
@@ -2,53 +2,45 @@ package com.acosta.ricardo.logindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.List;
+
 
 public class Werknemer extends AppCompatActivity {
 
-
-
-
-    ListView mListView;
-    Karwei[] job = new Karwei[2];
-
+    //LIST
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_werknemer);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_karweis);
 
-
-
-        //HARDCODED
-        job[0] = new  Karwei("Auto wassen","auto is vies, was het voor 50 euro","lol@gmail.com");
-        job[1] = new  Karwei("gras maaien","het gras staat hoog, maai dit af voor 50 euro","test@gmail.com");
-
-        final String[] karweitjes = {job[0].getTitle(),job[1].getTitle()};
-
-        mListView = (ListView) findViewById(R.id.listview);
-        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,karweitjes);
-        mListView.setAdapter(mAdapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        new FirebaseDatabaseHelper().readKarweis(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Karwei> karweis, List<String> keys) {
+                new RecyclerView_Config().setConfig(mRecyclerView,Werknemer.this,karweis,keys);
+            }
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void DataIsInserted() {
 
-                  //VERGELIJKT
-                  for(int i = 0; i < job.length;i++){
-                  if(karweitjes[position] == job[i].getTitle()){
-                      Toast.makeText(Werknemer.this,job[i].getDescription(),Toast.LENGTH_LONG).show();
-                  }
-                  //NIEUWE INTENT
+            }
 
+            @Override
+            public void DataIsUpdated() {
 
+            }
 
-                }
+            @Override
+            public void DataIsDeleted() {
 
             }
         });
